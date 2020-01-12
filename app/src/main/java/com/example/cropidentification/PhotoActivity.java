02c436ca6100a82bdf802a9209fc58dceac16901
sanket <2017.sanket.mahale@ves.ac.in>
2020-01-12 17:57:58 +0530

    new activity

--- app/src/main/java/com/example/cropidentification/PhotoActivity.java
+++ app/src/main/java/com/example/cropidentification/PhotoActivity.java
@@ -2,14 +2,38 @@ package com.example.cropidentification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    Button button2;
+    ImageView imageView;
+    TextView  textView;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        button2 = findViewById(R.id.button2);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,111);
+            }
+        });
 
     }
 }
