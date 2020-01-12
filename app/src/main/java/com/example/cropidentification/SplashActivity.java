e47aca31f4faac70283aa7650de80c47b9678846
Vedant Sonje <vedant.sonje12@gmail.com>
2020-01-12 18:52:25 +0530

    SplashActivity2

--- app/src/main/java/com/example/cropidentification/SplashActivity.java
+++ app/src/main/java/com/example/cropidentification/SplashActivity.java
@@ -2,13 +2,39 @@ package com.example.cropidentification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    TextView tvSplash;
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        tvSplash = findViewById(R.id.tvSplash);
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.a1);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                    Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
     }
 }
