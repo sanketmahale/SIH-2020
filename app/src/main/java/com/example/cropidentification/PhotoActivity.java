5ec133b6e566e1647cf9e1940dc8bc2a8781e8d8
sanket <2017.sanket.mahale@ves.ac.in>
2020-01-12 17:22:45 +0530

    new activity

---
+++ app/src/main/java/com/example/cropidentification/PhotoActivity.java
@@ -0,0 +1,15 @@
+package com.example.cropidentification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+
+    }
+}
